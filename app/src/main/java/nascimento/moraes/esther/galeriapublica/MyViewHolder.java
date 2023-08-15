8e46e1d99fe4b874e7a74d76ced1b41a7a8e8716
20211tiimi0052 <esther.ifes2021@gmail.com>
2023-08-15 08:44:03 -0300

    Página 26 - Passo 10 (teste)

---
+++ app/src/main/java/nascimento/moraes/esther/galeriapublica/MyViewHolder.java
@@ -0,0 +1,13 @@
+package nascimento.moraes.esther.galeriapublica;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import androidx.annotation.NonNull;
+
+public class MyViewHolder extends RecyclerView.ViewHolder {
+    public MyViewHolder(@NonNull View itemView){
+        super(itemView);
+    }
+}
